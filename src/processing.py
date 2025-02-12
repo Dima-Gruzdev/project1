16d247ad6ab64c781f86218448ec1569971a6249
gruzdevda <nubile4446@mail.ru>
2025-02-12 09:28:55 +0300

    add function filter

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+def filter_by_state(filter_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """Функция которая принимает на вход  список словарей и возращает новый отсортированный по ключу state"""
+    new_list_filter = []
+    for i in filter_list:
+        if i.get('state') == state:
+            new_list_filter.append(i)
+    return new_list_filter
+
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, ]))
+
+
+def sort_by_date():
+    pass
