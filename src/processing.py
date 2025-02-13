1b89791aef8b3b23fe5bddf830d344d217d8c39b
gruzdevda <nubile4446@mail.ru>
2025-02-13 09:45:55 +0300

    revision function

--- src/processing.py
+++ src/processing.py
@@ -16,7 +16,7 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, ]))
 
 
-def sort_by_date(sort_by_list: list[dict], rev: bool=True):
+def sort_by_date(sort_by_list: list[dict], rev: bool=True) -> list[dict]:
     """Функция которая сортирует список словарей по убыванию по дате"""
     return sorted(sort_by_list, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=rev)
 
