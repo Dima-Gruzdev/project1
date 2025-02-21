c439138e44f096138e615d8910d75a0951da7c21
gruzdevda <nubile4446@mail.ru>
2025-02-21 15:49:15 +0300

    modification func3

--- src/processing.py
+++ src/processing.py
@@ -22,18 +22,11 @@ print(
 )
 
 
-def sort_by_date(sort_by_list: list[dict], rev: bool = True) -> list[dict]:
+def sort_by_date(sort_by_list: list[dict], rev: bool = True) -> list[dict] | str:
     """Функция которая сортирует список словарей по убыванию по дате"""
-    return sorted(sort_by_list, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=rev)
+    try:
+        return sorted(sort_by_list, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=rev)
+    except ValueError:
+        return "Неверный формат даты"
 
-
-print(
-    sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+print(sort_by_date([]))
