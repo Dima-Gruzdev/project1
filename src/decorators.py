183f8e5fb56074f2a6384a81e6ea82df3ef90e37
gruzdevda <nubile4446@mail.ru>
2025-03-07 10:38:50 +0300

    add decorators.py and write func

---
+++ src/decorators.py
@@ -0,0 +1,36 @@
+from functools import wraps
+from typing import Optional
+
+
+def log(filename: Optional[any]) -> Optional[any]:
+    """Декаратор который автоматически  будет логировать начало и конец выполнения функции,
+     а также ее результаты или возникшие ошибки"""
+    def my_decarator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+                if filename is not None:
+                    with open(filename, "a") as file:
+                        file.write(f"\n{function.__name__} ok")
+                else:
+                    print(f"\n{function.__name__} ok")
+                return result
+            except Exception as err:
+                if filename is not None:
+                    with open(filename, "a") as file:
+                        file.write(f"\n{function.__name__} error: {err}. Inputs: {args}, {kwargs}")
+                else:
+                    print(f"\n{function.__name__} error: {err}. Inputs: {args}, {kwargs}")
+                raise err
+        return wrapper
+    return my_decarator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+
+if __name__ == "__main__":
+    print(my_function("2",2))
